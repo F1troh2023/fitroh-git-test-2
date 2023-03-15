df0aceedfa5c26a8adc8e5e9f2807283c309b0b1
F1troh2023 <fitroh@uinjkt.ac.id>
2023-03-15 22:05:56 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aliquid et eaque repellendus delectus veritatis, laudantium consectetur est possimus beatae. Cumque quibusdam animi placeat cum ratione vero perferendis modi accusamus.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
